06d909311e93d0184933c30c984d491a21d72828
Kyrowin <ten.ben2010@mail.ru>
2024-11-05 19:52:42 +0500

    123

--- bot.py
+++ bot.py
@@ -133,7 +133,7 @@ async def send_post(update: Update):
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         
-        await update.reply_media_group(media)
+        await update.message.reply_media_group(media)
         await update.reply_text(f"Тип животного: {post[1].get('animal_type', 'Неизвестно')}", reply_markup=reply_markup)
         
     else:
